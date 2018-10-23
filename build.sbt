7f9aaa88d75cbc292b5e6d8a850a13e618e58ec8
康壮 <kangzhuang@xiaoxinyong.com>
2018-10-23 14:29:07 +0800

    first commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "sparkML"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
---
+++ src/main/scala/test/day01.scala
@@ -0,0 +1,15 @@
+package test
+
+/**
+  * Author: KZ
+  * Date: 2018/10/23  14:10
+  */
+object day01 {
+
+  def main(args: Array[String]): Unit = {
+
+
+
+  }
+
+}
